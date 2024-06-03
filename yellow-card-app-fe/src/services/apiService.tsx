0b7ed83a53834778cb5866aa2b40a4f51efdcaf2
daniel-mawioo <mawioodaniel43@gmail.com>
2024-06-03 13:44:02 +0300

    update base url

--- yellow-card-app-fe/src/services/apiService.tsx
+++ yellow-card-app-fe/src/services/apiService.tsx
@@ -2,7 +2,7 @@
 import axios, { AxiosError } from "axios";
 
 // Set the base URL for the API
-const API_BASE_URL = "http://localhost:4000/api";
+const API_BASE_URL = "https://jellyfish-app-j7l4t.ondigitalocean.app/api";
 
 axios.defaults.baseURL = API_BASE_URL;
 
