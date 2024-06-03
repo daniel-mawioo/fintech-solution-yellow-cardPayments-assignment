a4f4b1ef1b3abfdad170a608bc8a1ec155a1e98a
daniel-mawioo <mawioodaniel43@gmail.com>
2024-06-03 14:18:34 +0300

    cors blocking issue fix

--- yellow-card-app-be/src/app.ts
+++ yellow-card-app-be/src/app.ts
@@ -9,7 +9,10 @@ const app = express();
 // Enable CORS for all routes
 app.use(
   cors({
-    origin: "http://localhost:4000", // Allow only this origin
+    origin: [
+      "http://localhost:4000",
+      "https://jellyfish-app-j7l4t.ondigitalocean.app",
+    ], // Allow these origins
     methods: "GET,POST,PUT,DELETE", // Allow these methods
     allowedHeaders: "Content-Type,Authorization", // Allow these headers
   })
